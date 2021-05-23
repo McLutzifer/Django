444fc137a561a2733d3d805ae8221ac7df308dbf
McLutzifer <lukasschumi@gmx.at>
2021-05-23 16:14:48 +0200

    rearange directories

--- real_Python/personal_portfolio/settings.py
+++ real_Python/personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z1=&y_vm@1^p5yn20%je%xbw429%(g0=q=*)fd%hmu)r8+g#r3'
+SECRET_KEY = 'django-insecure-6!-(aur2jtt@rr7nfzl40c@y3x%i59=#c=4a@4odx(76+0eif!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
