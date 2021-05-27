943c93d5f4d7c87dfc34b361f4ff73f819424e41
McLutzifer <lukasschumi@gmx.at>
2021-05-27 21:09:45 +0200

    add urlpatterns

--- LinkedIn/wisdompets/wisdompets/urls.py
+++ LinkedIn/wisdompets/wisdompets/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail'),
 ]
