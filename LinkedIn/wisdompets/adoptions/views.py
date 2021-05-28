cd9cf23351fdce0fb7b2872cd29dfb8bc6b67d84
McLutzifer <lukasschumi@gmx.at>
2021-05-28 17:21:55 +0200

    edit view

--- LinkedIn/wisdompets/adoptions/views.py
+++ LinkedIn/wisdompets/adoptions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 
 from .models import Pet
 
@@ -11,5 +11,11 @@ def home(request):
     #return HttpResponse('<p>home</p>')
 
 def pet_detail(request, pet_id):
-    
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')
+    return render(request, 'pet_detail.html', {
+        'pet':pet,
+    })
     #return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
