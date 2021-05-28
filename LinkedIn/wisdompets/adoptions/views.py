7bd2507bc817755aba5d2822ca6e29a4b1dad40b
McLutzifer <lukasschumi@gmx.at>
2021-05-28 17:19:20 +0200

    update views

--- LinkedIn/wisdompets/adoptions/views.py
+++ LinkedIn/wisdompets/adoptions/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Pet
+
 def home(request):
-    return HttpResponse('<p>home</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets':pets,
+    })
+    #return HttpResponse('<p>home</p>')
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
+    
+    #return HttpResponse(f'<p>pet_detail view with id {pet_id}</p>')
